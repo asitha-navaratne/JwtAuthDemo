ce279add45787c153de88f1f647982a7b735f129
Asitha Navaratne <asithan476@gmail.com>
2024-08-22 22:07:02 +0530

    Connected database

---
+++ backend/database/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Integer
+from .config import Base
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from typing import Annotated
+
+import database.models as models
+from database.config import engine, SessionLocal
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return "test"
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post('/users')
+async def get_all_users(db: db_dependency):
+    user = db.query(models.Users).all()
+
+    return user
